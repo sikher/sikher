01ffff141845f837f109ffeb5880518093c7a4ec
Jasdeep Khalsa <jasdeep@simplyspiritdesign.com>
2015-02-02 14:22:06 +0000

    Can now search by hitting the keyboard enter key

--- www/js/app.js
+++ www/js/app.js
@@ -127,4 +127,18 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/search');
 
+})
+
+.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
+
+                event.preventDefault();
+            }
+        });
+    };
 });
