bfcac6e522a6a3e87e03500385b3a55af90e6787
Jasdeep Khalsa <jasdeep@simplyspiritdesign.com>
2015-02-05 19:45:42 +0000

    Edited after build hook to only fire if its a --release

--- hooks/after_build/010_copy_build_to_dist.js
+++ hooks/after_build/010_copy_build_to_dist.js
@@ -2,17 +2,22 @@
 
 var fs = require('fs');
 var path = require('path');
+var cmd_args = /release/gi;
 
-console.log('argv', process.argv)
-console.log('env', process.env);
- 
-var rootdir = process.argv[2];
-var dist_dir = path.join(rootdir, 'dist');
+if(cmd_args.test(process.env.CORDOVA_CMDLINE))
+{
+	var rootdir = process.argv[2];
+	var dist_dir = path.join(rootdir, 'dist');
+	var build_files = [
+		[path.join(rootdir, 'platforms/android/ant-build/CordovaApp-release-unsigned.apk'), path.join(dist_dir, 'Sikher-release.apk')]
+	]
 
-var build_files = [
-	[path.join(rootdir, 'platforms/android/ant-build/CordovaApp-debug.apk'), path.join(dist_dir, 'Sikher-debug.apk')]
-]
-
-build_files.forEach(function(val, index, array) {
-	fs.createReadStream(val[0]).pipe(fs.createWriteStream(val[1]));
-});
+	build_files.forEach(function(val, index, array) {
+		fs.createReadStream(val[0]).pipe(fs.createWriteStream(val[1]));
+		console.log('Successfully copied', val[0], 'to', val[1])
+	});
+}
+else
+{
+	console.error('\nIf you want a copy of the build files in /dist then you must specify the --release flag when building e.g. cordova build --release');
+}
