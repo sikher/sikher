c1cec714c8875778e0f91a95d05fbf1db8b738db
Jasdeep Khalsa <jasdeep@simplyspiritdesign.com>
2015-10-29 20:37:48 +0000

    Modifying build script to copy debug version of android app, since the other version will not work unsigned

--- hooks/after_build/010_copy_build_to_dist.js
+++ hooks/after_build/010_copy_build_to_dist.js
@@ -9,7 +9,7 @@ if(cmd_args.test(process.env.CORDOVA_CMDLINE))
 	var rootdir = process.argv[2];
 	var dist_dir = path.join(rootdir, '../sikher-dist');
 	var build_files = [
-		[path.join(rootdir, 'platforms/android/build/outputs/apk/android-release-unsigned.apk'), path.join(dist_dir, 'Sikher-android.apk')]
+		[path.join(rootdir, 'platforms/android/build/outputs/apk/android-debug.apk'), path.join(dist_dir, 'Sikher-android.apk')]
 	]
 
 	build_files.forEach(function(val, index, array) {
