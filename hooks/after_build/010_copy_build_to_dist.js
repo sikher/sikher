969825149f9c0e2d9c7da6cebd221e81e88e2df7
Jasdeep Khalsa <jasdeep@simplyspiritdesign.com>
2015-11-04 21:06:49 +0000

    Updating Cordova build script to copy both debug and release versions of the app to the dist folder depending on if the --release flag is set or not

--- hooks/after_build/010_copy_build_to_dist.js
+++ hooks/after_build/010_copy_build_to_dist.js
@@ -3,21 +3,32 @@
 var fs = require('fs');
 var path = require('path');
 var cmd_args = /release/gi;
+var rootdir = process.argv[2];
+var dist_dir = path.join(rootdir, '../sikher-dist');
 
 if(cmd_args.test(process.env.CORDOVA_CMDLINE))
 {
-	var rootdir = process.argv[2];
-	var dist_dir = path.join(rootdir, '../sikher-dist');
+	console.log('Copying production release to '+dist_dir+'\n');
 	var build_files = [
-		[path.join(rootdir, 'platforms/android/build/outputs/apk/android-debug.apk'), path.join(dist_dir, 'Sikher-android.apk')]
+		[path.join(rootdir, 'platforms/android/build/outputs/apk/android-release-unsigned.apk'), path.join(dist_dir, 'android-release-unsigned.apk')]
 	]
 
 	build_files.forEach(function(val, index, array) {
 		fs.createReadStream(val[0]).pipe(fs.createWriteStream(val[1]));
-		console.log('Successfully copied', val[0], 'to', val[1])
+		console.log('Successfully copied', val[0], 'to', val[1]);
 	});
 }
 else
 {
-	console.error('\nIf you want a copy of the build files in /dist then you must specify the --release flag when building e.g. cordova build --release');
+	console.log('\nCopying debug release to '+dist_dir);
+	console.log('If you want a production release then you must specify the --release flag when building e.g. cordova build --release\n');
+
+	var build_files = [
+		[path.join(rootdir, 'platforms/android/build/outputs/apk/android-debug.apk'), path.join(dist_dir, 'android-debug.apk')]
+	]
+
+	build_files.forEach(function(val, index, array) {
+		fs.createReadStream(val[0]).pipe(fs.createWriteStream(val[1]));
+		console.log('Successfully copied', val[0], 'to', val[1]);
+	});
 }
