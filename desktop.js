f3471c7268cdc1bda0bd5596356b90cb4069f081
Jasdeep Khalsa <jasdeep@simplyspiritdesign.com>
2015-11-22 19:55:37 +0000

    Desktop app now has the ability to go fullscreen by pressing F11

--- desktop.js
+++ desktop.js
@@ -1,5 +1,6 @@
 var app = require('app');  // Module to control application life.
 var BrowserWindow = require('browser-window');  // Module to create native browser window.
+var globalShortcut = require('global-shortcut');
 
 // Report crashes to our server.
 require('crash-reporter').start();
@@ -7,6 +8,7 @@ require('crash-reporter').start();
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the javascript object is GCed.
 var mainWindow = null;
+var fullscreen = false;
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
@@ -20,10 +22,24 @@ app.on('ready', function() {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 1024, height: 768, icon:'resources/icon.png', 'web-preferences': { webaudio: true }});
 
+  // Register F11 shortcut to go toggle fullscreen
+  var ret = globalShortcut.register('F11', function() {
+    if(!fullscreen)
+    {
+          mainWindow.setFullScreen(true);
+          fullscreen = true;
+    }
+    else
+    {
+          mainWindow.setFullScreen(false);
+          fullscreen = false;
+    }
+  });
+
   // and load the index.html of the app.
   mainWindow.loadUrl('file://' + __dirname + '/www/index.html');
   mainWindow.maximize();
-  //mainWindow.openDevTools(); // For debugging
+  // mainWindow.openDevTools(); // For debugging
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
@@ -31,5 +47,10 @@ app.on('ready', function() {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null;
+  });
+
+  app.on('will-quit', function() {
+    // Unregister all shortcuts.
+    globalShortcut.unregisterAll();
   });
 });
