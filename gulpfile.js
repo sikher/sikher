b7c2a4096042aaac24dab66c64fdbe92e4b2a240
Sukhpal Singh <sukhpalsingh31@yahoo.com>
2016-04-06 14:56:21 +1000

    #12 Updating deprecated packages to fix the nbm install

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gutil = require('gulp-util');
 var bower = require('bower');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
+var cleanCss = require('gulp-clean-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
 
@@ -17,7 +17,7 @@ gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
     .pipe(sass())
     .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
+    .pipe(cleanCSS({
       keepSpecialComments: 0
     }))
     .pipe(rename({ extname: '.min.css' }))
